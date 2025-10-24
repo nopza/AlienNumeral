d90a2b2b23dc58a48d291cbf08edcccd7689cd15
nopza <nopzaknight@gmail.com>
2025-10-24 11:55:36 +0700

    [EDIT] : Update validate tsc test

--- tsc-alien-test/alien.js
+++ tsc-alien-test/alien.js
@@ -1,32 +1,73 @@
 function alienToInt(s) {
-  var alienValues = {
-    A: 1,
-    B: 5,
-    Z: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    R: 1000,
-  };
-  var total = 0;
-  for (var i = 0; i < s.length; i++) {
-    var char = s[i];
-
-    var value = alienValues[char];
-
-    var nextValue = 0;
-    if (i + 1 < s.length) {
-      var nextChar = s[i + 1];
-      nextValue = alienValues[nextChar];
+    var alienValues = {
+        A: 1,
+        B: 5,
+        Z: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        R: 1000,
+    };
+    var canSubtract = {
+        A: ['B', 'Z'], // 4, 9
+        Z: ['L', 'C'], // 40, 90
+        C: ['D', 'R'], // 400, 900
+        B: [], L: [], D: [], R: []
+    };
+    var total = 0;
+    var repeatCount = 1;
+    for (var i = 0; i < s.length; i++) {
+        var char = s[i];
+        var value = alienValues[char];
+        if (value === undefined) {
+            throw new Error("Invalid symbol: ".concat(char));
+        }
+        var nextChar = null;
+        var nextValue = 0;
+        if (i + 1 < s.length) {
+            nextChar = s[i + 1];
+            nextValue = alienValues[nextChar];
+        }
+        if (i > 0 && char === s[i - 1]) {
+            repeatCount++;
+            switch (char) {
+                case 'A':
+                case 'Z':
+                case 'C':
+                    if (repeatCount > 3) {
+                        throw new Error("Invalid: too many repetitions of ".concat(char));
+                    }
+                    break;
+                case 'B':
+                case 'L':
+                case 'D':
+                case 'R':
+                    throw new Error("Invalid: symbol ".concat(char, " cannot repeat"));
+            }
+        }
+        else {
+            repeatCount = 1;
+        }
+        if (value < nextValue) {
+            if (nextChar && canSubtract[char].indexOf(nextChar) !== -1) {
+                total -= value;
+                continue;
+            }
+            else {
+                throw new Error("Invalid: ".concat(char, " cannot precede ").concat(nextChar));
+            }
+        }
+        total += value;
     }
-    if (value < nextValue) {
-      total -= value;
-    } else {
-      total += value;
-    }
-  }
-  return total;
+    return total;
 }
-var s = "RCRZCAB";
-console.log("Input: s =", s);
-console.log("Output =", alienToInt(s));
+var samples = ["AAA", "LBAAA", "RCRZCAB", "AAAA", "AC", "AZ", "ZL", "ZR"];
+for (var _i = 0, samples_1 = samples; _i < samples_1.length; _i++) {
+    var s = samples_1[_i];
+    try {
+        console.log("Input: ".concat(s, " => Output: ").concat(alienToInt(s)));
+    }
+    catch (err) {
+        console.log("Input: ".concat(s, " => Error: ").concat(err.message));
+    }
+}
--- tsc-alien-test/alien.ts
+++ tsc-alien-test/alien.ts
@@ -1,7 +1,6 @@
 type AlienChar = 'A' | 'B' | 'Z' | 'L' | 'C' | 'D' | 'R';
 
 function alienToInt(s: string): number {
-
     const alienValues: Record<AlienChar, number> = {
         A: 1,
         B: 5,
@@ -12,30 +11,71 @@ function alienToInt(s: string): number {
         R: 1000,
     };
 
+    const canSubtract: Record<AlienChar, AlienChar[]> = {
+        A: ['B', 'Z'],   // 4, 9
+        Z: ['L', 'C'],   // 40, 90
+        C: ['D', 'R'],   // 400, 900
+        B: [], L: [], D: [], R: []
+    };
+
     let total = 0;
+    let repeatCount = 1;
 
     for (let i = 0; i < s.length; i++) {
-
         const char = s[i] as AlienChar;
-
         const value = alienValues[char];
 
+        if (value === undefined) {
+            throw new Error(`Invalid symbol: ${char}`);
+        }
+
+        let nextChar: AlienChar | null = null;
         let nextValue = 0;
         if (i + 1 < s.length) {
-            const nextChar = s[i + 1] as AlienChar;
+            nextChar = s[i + 1] as AlienChar;
             nextValue = alienValues[nextChar];
         }
 
-        if (value < nextValue) {
-            total -= value;
+        if (i > 0 && char === s[i - 1]) {
+            repeatCount++;
+            switch (char) {
+                case 'A':
+                case 'Z':
+                case 'C':
+                    if (repeatCount > 3) {
+                        throw new Error(`Invalid: too many repetitions of ${char}`);
+                    }
+                    break;
+                case 'B':
+                case 'L':
+                case 'D':
+                case 'R':
+                    throw new Error(`Invalid: symbol ${char} cannot repeat`);
+            }
         } else {
-            total += value;
+            repeatCount = 1;
         }
+
+        if (value < nextValue) {
+            if (nextChar && canSubtract[char].indexOf(nextChar) !== -1) {
+                total -= value;
+                continue;
+            } else {
+                throw new Error(`Invalid: ${char} cannot precede ${nextChar}`);
+            }
+        }
+
+        total += value;
     }
 
     return total;
 }
 
-const s = "RCRZCAB";
-console.log("Input: s =", s);
-console.log("Output =", alienToInt(s));
+const samples = ["AAA", "LBAAA", "RCRZCAB", "AAAA", "AC", "AZ", "ZL", "ZR"];
+for (const s of samples) {
+    try {
+        console.log(`Input: ${s} => Output: ${alienToInt(s)}`);
+    } catch (err: any) {
+        console.log(`Input: ${s} => Error: ${err.message}`);
+    }
+}
